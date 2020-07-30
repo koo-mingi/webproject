cfefcd2178c3789afc246a7271d0fc9e119656ba
mingikoo <ekzmfldks295@gmail.com>
2020-07-30 13:57:49 +0900

    홈

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		log.info("홈 페이지");
+		log.info("홈 페이지!");
 		
 		return "index";
 	}
