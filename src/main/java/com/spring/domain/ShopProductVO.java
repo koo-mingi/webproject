f1fc9a3c1b801583691a81899c4dcbde743a05b9
mingikoo <ekzmfldks295@gmail.com>
2020-08-04 17:38:39 +0900

    상품페이지 DB 적용

---
+++ src/main/java/com/spring/domain/ShopProductVO.java
@@ -0,0 +1,22 @@
+package com.spring.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class ShopProductVO {
+	private int pid;
+	private String pname;
+	private int price;
+	private int saleprice;
+	private int amount;
+	private String category;
+	private String description;
+	private int gradeavg;
+	private int volume;
+	private int shipcost;
+	private String storage;
+	private Date expdate;
+	private int imgid;
+}
---
+++ src/main/java/com/spring/mapper/ShopMapper.java
@@ -0,0 +1,9 @@
+package com.spring.mapper;
+
+import java.util.List;
+
+import com.spring.domain.ShopProductVO;
+
+public interface ShopMapper {
+	public List<ShopProductVO> select();
+}
